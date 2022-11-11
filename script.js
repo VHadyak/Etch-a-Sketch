084d9ffe7c30c50524321ee3f933189d3fd4d12e
vladh <hadyak305246@gmail.com>
2022-11-11 01:43:34 -0700

    Add a button that allows user to enter their own grid preference

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // 16x16 grid
 
-let columns = 16;
-let rows = 16;
+let userInput;
 
 const gridContainer = document.createElement("div");
 gridContainer.classList = ("grid-container");
 
-getGrid = () => {
+const gridGenerator = document.querySelector(".generate-grid");
+
+grid16x16 = () => {                                                                       // Default 16x16 Grid
+  let columns = 16;
+  let rows = 16;
+
   for (let i = 0; i < columns; ++i) {
     const column = document.createElement("div");
     column.classList = "column";
@@ -15,9 +19,46 @@ getGrid = () => {
       const row = document.createElement("div");
       row.classList = "row";
       column.appendChild(row);
+
+      row.addEventListener("mouseover", (e) => {
+        e.target.classList.add("grid-color");
+      });
     };
     gridContainer.appendChild(column);
   };
   document.body.appendChild(gridContainer);
 };
-getGrid();
+grid16x16();
+
+customGrid = () => {                                                                 // Creates a custom grid selected by user
+  for (let i = 0; i < userInput; ++i) {
+    if (userInput <= 100) {
+      const column = document.createElement("div");
+      column.classList = "column";
+
+      for (let j = 0; j < userInput; ++j) {
+        const row = document.createElement("div");
+        row.classList = "row";
+        column.appendChild(row);
+
+        row.addEventListener("mouseover", (e) => {
+          e.target.classList.add("grid-color");
+        });
+      };
+      gridContainer.appendChild(column);
+    } else {break;};
+  };
+  document.body.appendChild(gridContainer);
+};
+
+removeAllChildren = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  };
+};
+
+gridGenerator.addEventListener("click", () => {
+  removeAllChildren(gridContainer);
+  userInput = parseInt(prompt("Enter a number of squares to create a grid:", 1));
+  customGrid();
+});
