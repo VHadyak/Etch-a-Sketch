9f8fec5070eb65eec027f2b5ede53c2ebfddbbd0
vladh <hadyak305246@gmail.com>
2022-11-19 01:42:21 -0700

    Make grid container static, while grid squares are dynamic + add a slider

--- script.js
+++ script.js
@@ -1,64 +1,26 @@
-// 16x16 grid
 
-let userInput;
+const gridContainer = document.querySelector(".grid-container");
+const gridBtn = document.querySelector(".generate-grid");
+const slider = document.querySelector("#range")
+const value = document.querySelector("#value");
 
-const gridContainer = document.createElement("div");
-gridContainer.classList = ("grid-container");
 
-const gridGenerator = document.querySelector(".generate-grid");
 
-grid16x16 = () => {                                                                       // Default 16x16 Grid
-  let columns = 16;
-  let rows = 16;
 
-  for (let i = 0; i < columns; ++i) {
+slider.addEventListener("change", () => {
+  let userInput = slider.value;
+  gridContainer.innerHTML = "";
+
+  for (let i = 0; i < userInput; i++) {
     const column = document.createElement("div");
-    column.classList = "column";
+    column.classList.add("column");
 
-    for (let j = 0; j < rows; ++j) {
+    for (let j = 0; j < userInput; j++) {
       const row = document.createElement("div");
-      row.classList = "row";
+      row.classList.add("row");
       column.appendChild(row);
-
-      row.addEventListener("mouseover", (e) => {
-        e.target.classList.add("grid-color");
-      });
     };
     gridContainer.appendChild(column);
   };
   document.body.appendChild(gridContainer);
-};
-grid16x16();
-
-customGrid = () => {                                                                 // Creates a custom grid selected by user
-  for (let i = 0; i < userInput; ++i) {
-    if (userInput <= 100) {
-      const column = document.createElement("div");
-      column.classList = "column";
-
-      for (let j = 0; j < userInput; ++j) {
-        const row = document.createElement("div");
-        row.classList = "row";
-        column.appendChild(row);
-
-        row.addEventListener("mouseover", (e) => {
-          e.target.classList.add("grid-color");
-        });
-      };
-      gridContainer.appendChild(column);
-    } else {break;};
-  };
-  document.body.appendChild(gridContainer);
-};
-
-removeAllChildren = (parent) => {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  };
-};
-
-gridGenerator.addEventListener("click", () => {
-  removeAllChildren(gridContainer);
-  userInput = parseInt(prompt("Enter a number of squares to create a grid:", 1));
-  customGrid();
 });
