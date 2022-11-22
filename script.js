3bee936fcc78728dc9d09a01b092f283e2ae6f2c
vladh <hadyak305246@gmail.com>
2022-11-21 21:31:39 -0700

    Refactor the codebase to make it more readable

--- script.js
+++ script.js
@@ -5,41 +5,11 @@ const slider = document.querySelector("#range")
 const value = document.querySelector("#value");
 const resetBtn = document.querySelector(".reset");
 
-createDefaultGrid = () => {
-  value.textContent = "Grid size: " + 16 +"x"+16;                 
-  for (let i = 0; i < 16; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-
-    for (let j = 0; j < 16; j++) {
-      const row = document.createElement("div");
-      row.classList.add("row");
-      column.appendChild(row);
-    };
-    gridContainer.appendChild(column);
-  };
-  document.body.appendChild(gridContainer);
-
-  const gridCells = document.querySelectorAll(".row");
-  gridCells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-      cell.classList.add("cell-hover");
-    });
-  });
-
-  resetBtn.addEventListener("click", () => {
-    gridCells.forEach((cell) => {
-      cell.classList.remove("cell-hover");
-    });
-  });
-};
-createDefaultGrid();
-
-slider.addEventListener("change", () => {
+createGrid = () => {
   let userInput = slider.value;
-  value.textContent = `Grid size: ${userInput}x${userInput}`;
-  gridContainer.innerHTML = "";
-
+  gridContainer.textContent = "";
+  
+  value.textContent = `Grid size: ${userInput}x${userInput}`;           
   for (let i = 0; i < userInput; i++) {
     const column = document.createElement("div");
     column.classList.add("column");
@@ -54,15 +24,24 @@ slider.addEventListener("change", () => {
   document.body.appendChild(gridContainer);
 
   const gridCells = document.querySelectorAll(".row");
-  gridCells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-      cell.classList.add("cell-hover");
-    });
-  });
 
   resetBtn.addEventListener("click", () => {
     gridCells.forEach((cell) => {
       cell.classList.remove("cell-hover");
     });
   });
+  
+  gridCells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.classList.add("cell-hover");
+    });
+  });
+};
+createGrid();
+
+slider.addEventListener("change", () => {
+  createGrid();
 });
+
+
+
